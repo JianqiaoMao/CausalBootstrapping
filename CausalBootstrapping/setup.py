ee19c6d9751911457fc905afe2d47d1a3f52dcb8
Jianqiao Mao <maojianqiao001@gmail.com>
2024-02-02 18:45:15 +0000

    update for v0.1.1

--- CausalBootstrapping/setup.py
+++ CausalBootstrapping/setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='CausalBootstrapping',
-    version='0.1.0',
+    version='0.1.1',
     author='Jianqiao Mao',
     author_email='jxm1417@student.bham.ac.uk',
     license='GPL-3.0',
     description="CausalBootstrapping is an easy-access implementation and extention of causal bootstrapping (CB) technique for causal analysis. With certain input of observational data, causal graph and variable distributions, CB resamples the data by adjusting the variable distributions which follow intended causal effects.",
     url='https://github.com/JianqiaoMao/CausalBootstrapping',
-    packages = find_packages(include=["distEst_lib.py", "causalBootstrapping.py"]),
+    py_modules=['causalBootstrapping', 'distEst_lib'],
     install_requires=['numpy', 'grapl-causal'],
 )
